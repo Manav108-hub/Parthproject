e580f4b2f7e38472806e719d0d8541a58f2b28af
manav adwani <manavadwani86@example.com>
2024-05-27 15:35:02 +0530

    changes in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 "use client";
-
+import Link from "next/link";
 import React from "react";
 
 export default function Header() {
     return (
         <header className="container mx-auto">
-
+            <Link href = "/about">About</Link>
         </header>
     )
 }
