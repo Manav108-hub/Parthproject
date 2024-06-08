d5c48ec8f8a301212eb04efeb69d67f3df4eceac
manav adwani <manavadwani86@example.com>
2024-06-08 14:22:03 +0530

    latest commit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,15 +18,12 @@ function Navbar({className}: { className?: string }) {
             className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
         >
             <Menu setActive={setActive}>
-                <MenuItem setActive={setActive} active={active} item="Services">
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/web-dev">Web Development</HoveredLink>
-                        <HoveredLink href="/interface-design">Interface Design</HoveredLink>
-                        <HoveredLink href="/seo">Search Engine Optimization</HoveredLink>
-                        <HoveredLink href="/branding">Branding</HoveredLink>
-                    </div>
+            <MenuItem setActive={setActive} active={active} item="Home">
+            </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="About Us">
+
                 </MenuItem>
-                <MenuItem setActive={setActive} active={active} item="Products">
+                <MenuItem setActive={setActive} active={active} item="Catalog">
                     <div className="  text-sm grid grid-cols-2 gap-10 p-4">
                         <ProductItem
                             title="Algochurn"
@@ -54,14 +51,11 @@ function Navbar({className}: { className?: string }) {
                         />
                     </div>
                 </MenuItem>
-                <MenuItem setActive={setActive} active={active} item="Pricing">
-                    <div className="flex flex-col space-y-4 text-sm">
-                        <HoveredLink href="/hobby">Hobby</HoveredLink>
-                        <HoveredLink href="/individual">Individual</HoveredLink>
-                        <HoveredLink href="/team">Team</HoveredLink>
-                        <HoveredLink href="/enterprise">Enterprise</HoveredLink>
-                    </div>
+                <MenuItem setActive={setActive} active={active} item="Cart">
+
                 </MenuItem>
+                <MenuItem setActive={setActive} active={active} item="signup"></MenuItem>
+                <MenuItem setActive={setActive} active={active} item="Contact Us"></MenuItem>
             </Menu>
         </div>
     );
